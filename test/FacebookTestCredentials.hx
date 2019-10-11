5e0077842d8ce078519cfb8925be2f8a729aa783
ConstNW <constnw@gmail.com>
2019-10-11 20:30:22 +0300

    remove FacebookTestCredentials

--- test/FacebookTestCredentials.hx
+++ test/FacebookTestCredentials.hx
@@ -5,6 +5,6 @@ class FacebookTestCredentials
     /**
      * These must be filled out with valid Facebook app details for the tests to run.
      */
-    public static var appId = '449518272572053';
-    public static var appSecret = '3c1476083094ef90f1af9bb6f8a4d9b7';
+    public static var appId = '';
+    public static var appSecret = '';
 }
